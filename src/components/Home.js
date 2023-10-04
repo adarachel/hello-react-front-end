9fdb2b61a77abb5a63322f7b0992b2f2859043ea
Ada Rachel <adarachelofficial@gmail.com>
2023-10-03 19:21:17 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Home = () => (
-    <div>
-        <h1>Welcome to Greetings App!</h1>
-    </div>
+  <div>
+    <h1>Welcome to Greetings App!</h1>
+  </div>
 );
 
 export default Home;
